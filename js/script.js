29a6384c80d9a6782b9a3cafd6709a70829fd316
Daniell Mesquita <daniellmesquita@protonmail.com>
2021-08-01 01:59:33 -0300

    Init

---
+++ js/script.js
@@ -0,0 +1,133 @@
+(function ($) {
+	jQuery.expr[':'].Contains = function(a,i,m){return (a.textContent || a.innerText || "").toUpperCase().indexOf(m[3].toUpperCase())>=0;};
+	function listFilter(header, list,element) {
+		$(document).on("keyup",header, function() {
+			var filter = $(this).val();
+			if(filter) {
+				$(list).find(element+":not(:Contains(" + filter + "))").fadeOut("fast");
+				$(list).find(element+":Contains(" + filter + ")").fadeIn("fast");
+			} else {$(list).find(element).fadeIn();}
+			return false;
+		}).keyup(function() {$(this).change();});
+	}
+	$(function() {
+		listFilter("#storesearch", $("body"), ".view");
+	});
+}(jQuery)); 
+$(function() {
+	$("body,#installed").tabs();
+	$.each(apps, function(i,v) {
+		var id = "window_" + v.name.toLowerCase().replace(".","").replace(",","").replace(" ","");
+		var view = $("<div>").attr("class","view card");
+		var front = $("<div>").addClass("view_front").appendTo(view);
+		var icon = $("<img>").attr("src",v.icon).addClass("view_icon").appendTo(front);
+		$("<span>").attr("class","view_name").html(v.name).appendTo(front);
+		$("<span>").attr("class","view_author").html("by "+v.author).appendTo(front);
+		var back = $("<div>").addClass("view_back").appendTo(view);
+		$("<p>").addClass("view_desc").html(v.description).appendTo(back);
+		$("<button>").attr({"class":"view_install button blue small","data-url":v.manifest,"data-id":id}).html("<span>Install</span>").appendTo(back);
+		if(v.hdpiIconAvailable) {
+			var srcsetval = v.icon + " 1x, " + v.icon.substring(0,v.icon.length - 4) + "@2x.png 2x";
+			icon.attr("srcset",srcsetval);
+		}
+		$("#apps").append(view);
+	});
+	$.each(widgets, function(i,v) {
+		var id = "widget_" + v.name.toLowerCase().replace(".","").replace(",","").replace(" ","");
+		var result = '<div class="view card">'+
+		'<div class="view_front"><img src="'+v.icon+'" class="view_icon"/>'+
+		'<span class="view_name">'+v.name+'</span><span class="view_author">by '+v.author+'</span>'+
+		'</div>'+
+		'<div class="view_back">'+
+			'<p class="view_desc">'+v.description+'</p>'+
+			'<button class="view_install button blue small" data-url="'+v.url+'" data-id="'+id+'"><span>Install</span></button>'+
+		'</div>'+
+		'</div>';
+		$("#widgets").append(result);
+	});
+	$(document).on("app-change", function(e) {
+		$("#apps .view_install").each(function() {
+			var id = $(this).attr("data-id");
+			if(parent.apps.check(id,"installed")) {
+				$(this).addClass("installed green").removeClass("blue");
+			}
+			else {
+				$(this).addClass("blue").removeClass("installed green");
+			}
+		});
+		$("#installedapps ul").empty();
+		if(parent.apps.list("custom","name").length) {
+			$.each(parent.apps.list("custom","name"), function(i,data) {
+				var id = "window_" + data.toLowerCase().replace(".","").replace(",","").replace(" ","");
+				var li = $("<li>").attr("data-id",id).addClass("card");
+				$("<span>").attr("class","title").html(data).appendTo(li);
+				li.append("<span class='buttons'><button class='button green small button_launch'>Launch</button></span></li>");
+				$("#installedapps ul").append(li);
+			});
+		}
+	});
+	$(document).on("widget-change", function() {
+		$("#widgets .view_install").each(function() {
+			var id = $(this).attr("data-id");
+			if(parent.widgets.isinstalled(id)) {
+				$(this).addClass("installed red").removeClass("blue");
+			}
+			else {
+				$(this).addClass("blue").removeClass("installed red");
+			}
+		});
+		$("#installedwidgets ul").empty();
+		if(parent.widgets.list().length) {
+			$.each(parent.widgets.list(), function(i,data) {
+				var id = "widget_" + data.toLowerCase().replace(".","").replace(",","").replace(" ","");
+				var li = $("<li>").attr("data-id",id).addClass("card");
+				$("<span>").attr("class","title").html(data).appendTo(li);
+				li.append("<span class='buttons'><button class='button blue small button_reload'>Reload</button><button class='button red small button_remove'>Remove</button></span></li>");
+				$("#installedwidgets ul").append(li);
+			});
+		}
+	});
+	$(document).on("social-change", function() {
+		$("#social ul").empty();
+		if(parent.social.list().length) {
+			$.each(parent.social.list(),function(i,data) {
+				var li = $("<li>").attr("data-id",data.app).addClass("card");
+				var buttons = $("<span>").attr("class","buttons");
+				$("<span>").attr("class","title").html(parent.apps.getInfo(data.app,"name")).appendTo(li);
+				buttons.append("<button class='button red small button_uninstall'>Uninstall</button>").appendTo(li);
+				$("#social ul").append(li);
+				if(parent.social.checkInstall(data.app,"sidebar")) {
+					buttons.prepend("<span class='indicator sidebar'></span>");
+				}
+				if(parent.social.checkInstall(data.app,"share")) {
+					buttons.prepend("<span class='indicator share'></span>");
+				}
+			});
+		}
+	});
+	$(document).trigger("app-change").trigger("widget-change").trigger("social-change");
+	$(document).on("click","#apps .view_install:not(.installed)",function() {
+		parent.apps.install($(this).attr("data-url"));
+	});
+	$(document).on("click","#widgets .view_install:not(.installed)",function() {
+		parent.widgets.install($(this).attr("data-url"), $(this).parent().siblings(".view_front").children(".view_name").html())
+	});
+	$(document).on("click","#apps .view_install.installed",function() {
+		parent.apps.open($(this).attr("data-id"));
+	});
+	$(document).on("click","#widgets .view_install.installed",function() {
+		parent.widgets.remove($(this).attr("data-id"));
+	});
+	$(document).on("click","#installedwidgets li .buttons .button_reload",function() {
+		parent.widgets.reload($(this).parent().parent().attr("data-id"));
+	});
+	$(document).on("click","#installedwidgets li .buttons .button_remove",function() {
+		parent.widgets.remove($(this).parent().parent().attr("data-id"));
+	});
+	$(document).on("click","#installedapps li .buttons .button_launch",function() {
+		parent.apps.open($(this).parent().parent().attr("data-id"));
+	});
+	$(document).on("click","#social .button_uninstall",function() {
+		parent.social.unregister($(this).parent().parent().attr("data-id"));
+	});
+});
---
+++ listing.js
@@ -0,0 +1,197 @@
+var apps = [
+    {
+        "name": "API Demo",
+        "author": "Tim",
+        "icon": "../../img/icons/developer.png",
+        "manifest": "manifests/apidemo.cdapp",
+        "description": "An app made to test CloudDesk API.",
+        "hdpiIconAvailable": true
+    },
+    {
+        "name": "Aviary",
+        "author": "Tim",
+        "icon": "../../img/icons/aviary.png",
+        "manifest": "manifests/aviary.cdapp",
+        "description": "An image editor powered by Aviary",
+        "hdpiIconAvailable": true
+    },
+    {
+        "name": "Bit.ly",
+        "author": "Tim",
+        "icon": "../../img/icons/bitly.png",
+        "manifest": "manifests/bitly.cdapp",
+        "description": "An bit.ly app to shorten your links",
+        "hdpiIconAvailable": true
+    },
+    {
+        "name": "Browser",
+        "author": "Tim",
+        "icon": "../../img/icons/browser.png",
+        "manifest": "manifests/browser.cdapp",
+        "description": "An app made to surf the web.",
+        "hdpiIconAvailable": true
+    },
+    {
+        "name": "Code Tester",
+        "author": "Tim",
+        "icon": "../../img/icons/codetester.png",
+        "manifest": "manifests/codetester.cdapp",
+        "description": "A awesome app to test your code.",
+        "hdpiIconAvailable": true
+    },
+    {
+        "name": "Delicious",
+        "author": "Delicious",
+        "icon": "../../img/icons/delicious.png",
+        "manifest": "manifests/delicious.cdapp",
+        "description": "An app to manage your delicious bookmarks.",
+        "hdpiIconAvailable": true
+    },
+    {
+        "name": "Finance",
+        "author": "simobortolo",
+        "icon": "../../img/icons/finance.png",
+        "manifest": "manifests/finance.cdapp",
+        "description": "Keep in track of the economy using this Finance app.",
+        "hdpiIconAvailable": true
+    },
+    {
+        "name": "Flickr",
+        "author": "Tim",
+        "icon": "../../img/icons/flickr.png",
+        "manifest": "manifests/flickr.cdapp",
+        "description": "A Flickr app to search beautiful pictures.",
+        "hdpiIconAvailable": true
+    },
+    {
+        "name": "Goal",
+        "author": "Goal",
+        "icon": "../../img/icons/goal.png",
+        "manifest": "manifests/goal.cdapp",
+        "description": "A Goal app to follow the latest football news.",
+        "hdpiIconAvailable": true
+    },
+    {
+        "name": "Google Drive",
+        "author": "Carlos Jeurissen",
+        "icon": "../../img/icons/drive.png",
+        "manifest": "manifests/googledrive.cdapp",
+        "description": "A Google Drive app to manage your files online",
+        "hdpiIconAvailable": true
+    },
+    {
+        "name": "Markpad",
+        "author": "Tim",
+        "icon": "../../img/icons/markpad.png",
+        "manifest": "manifests/markpad.cdapp",
+        "description": "A text editor that supports markdown.",
+        "hdpiIconAvailable": true
+    },
+    {
+        "name": "Photos",
+        "author": "Tim",
+        "icon": "../../img/icons/photos.png",
+        "manifest": "manifests/photos.cdapp",
+        "description": "An app to view your photos.",
+        "hdpiIconAvailable": true
+    },
+    {
+        "name": "SmartSearch",
+        "author": "Tim",
+        "icon": "../../img/icons/smartsearch.png",
+        "manifest": "manifests/smartsearch.cdapp",
+        "description": "A beautiful search engine that allows you to compare seemlessly across engines.",
+        "hdpiIconAvailable": true
+    },
+    {
+        "name": "Task Manager",
+        "author": "Tim",
+        "icon": "../../img/icons/taskmanager.png",
+        "manifest": "manifests/taskmanager.cdapp",
+        "description": "An app to manage running apps.",
+        "hdpiIconAvailable": true
+    },
+    {
+        "name": "Todo list",
+        "author": "Tim",
+        "icon": "../../img/icons/todolist.png",
+        "manifest": "manifests/todolist.cdapp",
+        "description": "A to-do list to get your day organized.",
+        "hdpiIconAvailable": true
+    },
+    {
+        "name": "Unit Converter",
+        "author": "simobortolo",
+        "icon": "../../img/icons/unitconverter.png",
+        "manifest": "manifests/unitconverter.cdapp",
+        "description": "An app to easily convert your units.",
+        "hdpiIconAvailable": true
+    },
+    {
+        "name": "Vine",
+        "author": "Tim",
+        "icon": "../../img/icons/vine.png",
+        "manifest": "manifests/vine.cdapp",
+        "description": "Explore the world of vines using this app.",
+        "hdpiIconAvailable": true
+    },
+    {
+        "name": "Wikipedia",
+        "author": "Wikipedia",
+        "icon": "../../img/icons/wikipedia.png",
+        "manifest": "manifests/wikipedia.cdapp",
+        "description": "A wikipedia app to increase your knowledge",
+        "hdpiIconAvailable": true
+    }
+];
+var widgets = [
+    {
+        "name": "Battery",
+        "author": "Tim",
+        "icon": "img/battery.png",
+        "url": "ajax/widgets/battery/index.html",
+        "description": "A widget to get your battery level on your desktop."
+    },
+    {
+        "name": "Desk Seeker",
+        "author": "Tim",
+        "icon": "img/deskseeker.png",
+        "url": "ajax/widgets/seeker/index.html",
+        "description": "A widget that shows you an overview of your desktop."
+    },
+    {
+        "name": "Notes",
+        "author": "Tim",
+        "icon": "../../img/icons/notepad.png",
+        "url": "ajax/widgets/notes/index.html",
+        "description": "A sticky notes app for your desktop."
+    },
+    {
+        "name": "PassLock",
+        "author": "Tim",
+        "icon": "img/passlock.png",
+        "url": "ajax/widgets/passlock/index.html",
+        "description": "A widget that allows you to add an encrypted password to CloudDesk."
+    },
+    {
+        "name": "Styler",
+        "author": "Tim",
+        "icon": "img/styler.png",
+        "url": "ajax/widgets/styler/index.html",
+        "description": "An addon to style CloudDesk using CSS."
+    },
+    {
+        "name": "TouchPad",
+        "author": "Tim",
+        "icon": "img/placeholder.png",
+        "url": "ajax/widgets/touchpad/index.html",
+        "description": "A touchpad with many functionalities."
+    },
+    {
+        "name": "Weather",
+        "author": "DaKoder",
+        "icon": "img/weather.png",
+        "url": "ajax/widgets/weather/index.html",
+        "description": "A widget to get your weather right on your desktop."
+    }
+];
